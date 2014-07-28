d023a3eb76f473ed6cda2244eb28da5e80b35429
Justin McNally <justin@kohactive.com>
2014-07-28 13:46:39 -0500

    fix gallery

--- lib/assetable/inputs/uploaders.rb
+++ lib/assetable/inputs/uploaders.rb
@@ -9,7 +9,7 @@ module Assetable::Uploaders
       # modal_gallery.assets.each do |asset|
       modal_gallery.asset_attachments.each_with_index do |asset_attachment, index|
         # gallery += asset_preview(asset, :gallery, fieldname) 
-        gallery += ac.new.send(:render_to_string, partial: "assetable/shared/templates/gallery_item", locals: { asset: asset_attachment.asset, asset_attachment: asset_attachment, fieldname: fieldname, index: index })
+        gallery += ac.send(:render_to_string, partial: "assetable/shared/templates/gallery_item", locals: { asset: asset_attachment.asset, asset_attachment: asset_attachment, fieldname: fieldname, index: index })
       end
 
       gallery
